1f03d618c854629a01557f4a8319e6363d039304
marcus-peterson <marcus.peterson.jobb@gmail.com>
2023-04-20 14:12:01 +0200

    LAde till requirments för projektet

--- herr_schema.py
+++ herr_schema.py
@@ -9,47 +9,58 @@ import pytz
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents, case_insensitive=True)
 
-def get_schedule(url, date):
+
+def get_schedule(url, start_date, days=1):
     ics = requests.get(url).text
     cal = Calendar.from_ical(ics)
     local_timezone = pytz.timezone("Europe/Stockholm")
-    
+
     events = []
-    for event in cal.walk("vevent"):
-        start = event.get("dtstart").dt.astimezone(local_timezone)
-        end = event.get("dtend").dt.astimezone(local_timezone)
-        if start.date() == date:
-            summary = event.get("summary")
-            time_range = f"{start.strftime('%H:%M')} - {end.strftime('%H:%M')}"
-            events.append((time_range, summary))
-    
+    for day in range(days):
+        current_date = start_date + timedelta(days=day)
+        daily_events = []
+        for event in cal.walk("vevent"):
+            start = event.get("dtstart").dt.astimezone(local_timezone)
+            end = event.get("dtend").dt.astimezone(local_timezone)
+            if start.date() == current_date:
+                summary = event.get("summary")
+                time_range = f"{start.strftime('%H:%M')} - {end.strftime('%H:%M')}"
+                daily_events.append((time_range, summary))
+        events.append((current_date, daily_events))
+
     return events
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} has connected to Discord!")
     daily_schedule.start()
 
+
 @tasks.loop(hours=24)
 async def daily_schedule():
-    channel_id = 123  # Replace with your channel ID
+    channel_id = 1098570053735174155  # Replace with your channel ID
     channel = bot.get_channel(channel_id)
-    
+
     local_timezone = pytz.timezone("Europe/Stockholm")
     today = datetime.now(local_timezone).date()
-    
-    events = get_schedule("https://cloud.timeedit.net/nackademin/web/1/ri65E5ZQ1Q48eCQZ05Q7dtDQZ58ZZ5D9BuAYZ6Qy875tAnFb0FCQ74DF35585AD8B23AA2D4.ics", today)
-    
+
+    events = get_schedule(
+        "https://cloud.timeedit.net/nackademin/web/1/ri65E5ZQ1Q48eCQZ05Q7dtDQZ58ZZ5D9BuAYZ6Qy875tAnFb0FCQ74DF35585AD8B23AA2D4.ics",
+        today)
+
     if events:
         message = f"Dagens schema ({today.strftime('%Y-%m-%d')} - {today.strftime('%A')}):\n"
-        for time_range, summary in events:
+        for time_range, summary in events[0][1]:
             message += f"{time_range}: {summary}\n"
     else:
         message = f"Inga händelser idag ({today.strftime('%Y-%m-%d')} - {today.strftime('%A')})."
-    
+
     await channel.send(message)
+
 
 @bot.command()
 async def schema(ctx, date_str=None):
@@ -63,15 +74,44 @@ async def schema(ctx, date_str=None):
             await ctx.send("Ange datum i formatet YYYY-MM-DD.")
             return
 
-    events = get_schedule("https://cloud.timeedit.net/nackademin/web/1/ri65E5ZQ1Q48eCQZ05Q7dtDQZ58ZZ5D9BuAYZ6Qy875tAnFb0FCQ74DF35585AD8B23AA2D4.ics", date)
+    events = get_schedule(
+        "https://cloud.timeedit.net/nackademin/web/1/ri65E5ZQ1Q48eCQZ05Q7dtDQZ58ZZ5D9BuAYZ6Qy875tAnFb0FCQ74DF35585AD8B23AA2D4.ics",
+        date)
 
     if events:
         message = f"Schema för {date.strftime('%Y-%m-%d')} - {date.strftime('%A')}:\n"
-        for time_range, summary in events:
+        for time_range, summary in events[0][1]:
             message += f"{time_range}: {summary}\n"
     else:
         message = f"Inga händelser för {date.strftime('%Y-%m-%d')} - {date.strftime('%A')}."
     await ctx.send(message)
 
+@bot.command()
+async def veckoschema(ctx):
+    local_timezone = pytz.timezone("Europe/Stockholm")
+    today = datetime.now(local_timezone).date()
+    weekday = today.weekday()
+
+    start_date = today - timedelta(days=weekday)
+
+    events = get_schedule(
+        "https://cloud.timeedit.net/nackademin/web/1/ri65E5ZQ1Q48eCQZ05Q7dtDQZ58ZZ5D9BuAYZ6Qy875tAnFb0FCQ74DF35585AD8B23AA2D4.ics",
+        start_date, days=7)
+
+    message = "Veckans schema:\n"
+    for date, daily_events in events:
+        if daily_events:
+            message += f'{42*"-"}\n'
+            message += f"{date.strftime('%Y-%m-%d')} - {date.strftime('%A')}:\n"
+            for time_range, summary in daily_events:
+                message += f"{time_range}: {summary}\n"
+                
+                
+        else:
+            message += f'{42*"-"}\n'
+            message += f"Inga händelser för {date.strftime('%Y-%m-%d')} - {date.strftime('%A')}.\n"
+    await ctx.send(f"{message}")
+
 # Replace "YOUR_DISCORD_BOT_TOKEN" with your actual bot token
-bot.run("token")
+bot.run("MTA3ODYwMjI1NTYzMDA5MDI5MA.GsfJyi.DJwSr47bo0pinhTS_KQFg1ysm0phLTSzPXbRxA")
+
